6d9346798540ae8282a3149d618b511bc7125d8f
anjanianusuri <38336093+anjanianusuri@users.noreply.github.com>
2019-05-14 15:02:41 +1000

    venue profile updates

--- private/queries.php
+++ private/queries.php
@@ -11,5 +11,31 @@ venue_name TINYTEXT NOT NULL,
 venue_desc LONGTEXT NOT NULL,
 venue_phone int(12) NOT NULL);
 
+CREATE TABLE courts(
+venue_id int(11) REFERENCES venue(venue_id),
+court_id int(11) PRIMARY KEY NOT NULL,
+court_name TINYTEXT NOT NULL
+);
+
+CREATE TABLE timings(
+court_id int(11) REFERENCES court(court_id),
+timing_id int(11) PRIMARY KEY NOT NULL,
+time_range TIME NOT NULL
+);
+
+CREATE TABLE coaches(
+venue_id int(11) REFERENCES venue(venue_id),
+coach_id int(11) PRIMARY KEY NOT NULL,
+coach_name TINYTEXT NOT NULL,
+coach_desc LONGTEXT NOT NULL
+);
+
 INSERT INTO `venue`(`venue_id`, `signupid`, `venue_name`, `venue_desc`, `venue_phone`) VALUES (1,1,'St Lucia Court','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 ',0435661027)
+
+INSERT INTO `courts`(`venue_id`, `court_id`, `court_name`) VALUES (1,1, 'Green Court');
+
+INSERT INTO `timings`(`court_id`, `timing_id`, `time_range`) VALUES (1, 1, '9.00 - 10.00');
+INSERT INTO `timings`(`court_id`, `timing_id`, `time_range`) VALUES (1, 2, '10.00 - 11.00');
+
+INSERT INTO `coaches`(`venue_id`, `coach_id`, `coach_name`, `coach_desc`) VALUES (1, 1, 'Bhargav Bandi', 'Abcd');
--- public/venueprofile.php
+++ public/venueprofile.php
@@ -12,10 +12,38 @@ require_once ("../private/initialize.php");
 
         $id = $_SESSION['id'];
 
-        $sql = "select * from venue where signupid='$id'";
-        $result = mysqli_query($conn, $sql);
-        $row = mysqli_fetch_assoc($result);
+        $sql_1 = "select * from venue where signupid='$id'";
+        $result_1 = mysqli_query($conn, $sql_1);
+        $row_1 = mysqli_fetch_assoc($result_1);
 
-            echo $row['venue_name'];
+        $venue_id = $row_1['venue_id'];
+
+        $sql_2 = "select * from courts where venue_id='$venue_id'";
+        $result_2 = mysqli_query($conn, $sql_2);
+        $row_2 = mysqli_fetch_assoc($result_2);
+
 
     }
+?>
+<div class="container">
+    <h1>My Profile</h1>
+    <hr>
+    <h2><?php echo $row_1['venue_name'];?></h2>
+    <table>
+        <tr>
+            <td><strong>Description: </strong></td>
+            <td class="profiletable"><?php echo $row_1['venue_desc']?></td>
+        </tr>
+        <tr>
+            <td><strong>Phone: </strong></td>
+            <td class="profiletable"><?php echo $row_1['venue_phone'] ?></td>
+        </tr>
+    </table>
+    <hr>
+    <h2>Courts</h2>
+    <table>
+        <td><strong>Court Name:</strong></td>
+        <td class="profiletable"><?php echo $row_2['court_name'] ?></td>
+    </table>
+
+</div>
