8c1b3d7a3a076e2cae57033c7af569a8d151a294
Bhargavbandi <38348586+Bhargavbandi@users.noreply.github.com>
2019-05-24 15:21:18 +1000

    Update booking.php

--- public/booking.php
+++ public/booking.php
@@ -1,1 +1,11 @@
 <?php
+    $page_title = 'Home';
+    require_once ("../private/initialize.php");
+    include (SHARED_PATH.'/header.php');
+
+    require_once ("../private/initialize.php");
+    require_once ("../private/database.php");
+
+    ?>
+
+
